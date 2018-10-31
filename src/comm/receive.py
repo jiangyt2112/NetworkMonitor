dfaba5440de9c36b8fea96aa40db6d24331de1ae
jiangyt2112 <946692706@qq.com>
2018-10-31 15:56:19 +0800

    增加组件通信功能

--- src/comm/receive.py
+++ src/comm/receive.py
@@ -5,12 +5,17 @@ connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 
 channel.queue_declare(queue='hello')
-
+channel.queue_declare(queue='hello1')
 def callback(ch, method, properties, body):
     print(" [x] Received %r" % body)
+def callback1(ch, method, properties, body):
+    print(" [x1] Received %r" %body)
 
 channel.basic_consume(callback,
                       queue='hello',
+                      no_ack=True)
+channel.basic_consume(callback1,
+                      queue='hello1',
                       no_ack=True)
 
 print(' [*] Waiting for messages. To exit press CTRL+C')
--- src/comm/send.py
+++ src/comm/send.py
@@ -5,8 +5,12 @@ connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 
 channel.queue_declare(queue='hello')
+channel.queue_declare(queue='hello1')
 channel.basic_publish(exchange='',
                       routing_key='hello',
                       body='Hello World!')
+channel.basic_publish(exchange='',
+                      routing_key='hello1',
+                      body='Hello World1!')
 print(" [x] Sent 'Hello World!'")
 connection.close()
