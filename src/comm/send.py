c663bb8413d5505b42eb423cc2e69d468c5f4a4e
jiangyt2112 <946692706@qq.com>
2018-10-31 15:48:21 +0800

    增加组件通信功能

--- src/comm/send.py
+++ src/comm/send.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python2
 import pika
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('10.10.150.28'))
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
+
+channel.queue_declare(queue='hello')
+channel.basic_publish(exchange='',
+                      routing_key='hello',
+                      body='Hello World!')
+print(" [x] Sent 'Hello World!'")
+connection.close()
