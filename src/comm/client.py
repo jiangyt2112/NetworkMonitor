15d4c6effae41ccf38427934f5ac1a6c754ccff8
jiangyt2112 <946692706@qq.com>
2018-10-31 17:39:55 +0800

    增加基础组件通信功能

---
+++ src/comm/client.py
@@ -0,0 +1,105 @@
+#!/usr/bin/python2
+
+import pika
+import sys
+import uuid
+
+class Client(object):
+    def __init__(self, exchange, routing_key, message = "", username = 'network_monitor', passwd = '111111', vhost = "network_monitor"
+                host = '192.168.122.1', port = 5672):
+        self.credentials = pika.PlainCredentials(username, passwd)
+        # self.connection = pika.BlockingConnection(pika.ConnectionParameters(host, port, '/', credentials))
+        # self.channel = self.connection.channel()
+        self.host = host
+        self.port = port
+        self.vhost = vhost
+        self.message = str(message)
+        self.exchange = exchange
+        self.routing_key = routing_key
+
+    def connect(self):
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(self.host, self.port, self.vhost, self.credentials))
+        self.channel = self.connection.channel()
+
+    def rpc_call(self):
+        result = self.channel.queue_declare(exclusive=True)
+        self.callback_queue = result.method.queue
+        self.channel.basic_consume(self.on_response, no_ack=True,
+                            queue= self.callback_queue) 
+
+    def on_response(self, ch, method, props, body):
+        if self.corr_id == props.correlation_id:
+            # print body 
+            self.response = body
+
+    def __call__(self):
+        try:
+            self.connect()
+        except Exception, e:
+            print str(e)
+            self.close()
+            return False, str(e)
+
+        self.response = None
+        if self.routing_key.split('.')[1] == rpc:
+            self.corr_id = str(uuid.uuid4())
+            try:
+                self.channel.basic_publish(exchange = self.exchange,
+                                    routing_key = self.routing_key,
+                                    properties = pika.BasicProperties(
+                                                    reply_to = self.callback_queue,
+                                                    correlation_id = self.corr_id),
+                                    body = self.message)
+            
+                while self.response is None:
+                    self.connection.process_data_events()
+                self.close()
+            except Exception, e:
+                print str(e)
+                self.close()
+                return False, str(e)
+            return True, self.response
+        else:
+            try:
+                self.channel.basic_publish(exchange = self.exchange,
+                                        routing_key = self.routing_key,
+                                        body = self.message)
+                self.close()
+            except Exception, e:
+                print str(e)
+                self.close()
+                return False, str(e)
+            return True, None
+
+    def close(self):
+        print "close connection"
+        try:
+            self.channel.close()
+            self.connection.close()
+        except Exception, e:
+            pass
+
+    def __del__(self):
+        pass
+
+def start_re_rpc(vm_id):
+    # start resource evaluation rpc
+    req_id = "req-" + str(uuid.uuid4())
+    msg = {"req_id": req_id, "vm_id": vm_id}
+    rpc_client("network_monitor", "start_re", msg)
+    
+
+def end_re_rpc(vm_id):
+    # end resource evaluation rpc
+    req_id = "req-" + str(uuid.uuid4())
+    msg = {"req_id": req_id, "vm_id": vm_id}
+    rpc_client("network_monitor", "end_re", msg)
+
+if __name__ == "__main__":
+    req_id = str(uuid.uuid4())
+    vm_id = str(uuid.uuid4()) 
+    msg = {"req_id": req_id, "vm_id": vm_id}
+    rpc_client = RpcClient("resource_evaluation", "start_re", msg)
+    rpc_client()
+    # rpc_client()
+
---
+++ src/comm/server.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python2
+import __init__
+from utils.log import FALOG
+import time
+import pika
+import sys
+
+class Server(object):
+    def __init__(self, exchange, binding_keys, username = 'network_monitor', passwd = '111111', vhost = 'network_monitor'
+                host = '192.168.122.1', port = 5672):
+
+        credentials = pika.PlainCredentials(username, passwd)
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host, port, vhost, credentials))
+        self.channel = self.connection.channel()
+
+        self.channel.exchange_declare(exchange= exchange,
+                                exchange_type='topic')
+
+        result = self.channel.queue_declare(exclusive=True)
+        queue_name = result.method.queue
+
+
+        for binding_key in binding_keys:
+            self.channel.queue_bind(exchange=exchange,
+                                    queue=queue_name,
+                                    routing_key=binding_key)
+
+        print(' [*] Waiting for logs. To exit press CTRL+C')
+
+        #self.callback()
+        self.channel.basic_qos(prefetch_count = 1)
+        self.channel.basic_consume(self.callback,
+                               queue=queue_name)
+
+    def run(self):
+        try:
+            self.channel.start_consuming() 
+        except Exception, e:
+            print str(e)
+            FALOG.error("network-monitor service down:%s" %(str(e)))
+            sys.exit(1)
+
+    def callback(self, ch, method, props, body):
+        """
+            body:message
+            properties:prop.reply_to
+        """
+        if method.routing_key == "api_to_server":
+            # other process
+            response = {
+                        "task_type": "start_re",
+                        "exe_res": True,
+                        "error_msg": "",
+                        #"req_id": eval(body)['req_id'],
+                        #"vm_id": eval(body)['vm_id'],
+                        #"task_start_time": 
+                        #time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(eval(body)['start_time']))
+                        }
+
+        elif method.routing_key == "agent_to_server":
+            # other process
+            response = {
+                        "task_type": "end_re",
+                        "exe_res": True,
+                        "error_msg": "",
+                        #"req_id": eval(body)['req_id'],
+                        #"vm_id": eval(body)['vm_id'],
+                        #"task_end_time": 
+                        #time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(eval(body)['end_time']))
+                        }
+        else:
+            response = "no routing_key"
+ 
+        ch.basic_publish(exchange='',
+                 routing_key=props.reply_to,
+                 properties=pika.BasicProperties(correlation_id = \
+                                                     props.correlation_id),
+                 body=str(response))
+        ch.basic_ack(delivery_tag = method.delivery_tag)
+        print(" [x] %r:%r" % (method.routing_key, body))
+
+
+class T(RpcServer):
+    def __init__(self, exchange, binding_keys):
+        super(T, self).__init__(exchange, binding_keys)
+    #def callback(self):
+    #    print "aaaaa"
+    # override callback    
+
+if __name__ == "__main__":
+    rpc_server = RpcServer("network_monitor_server", ["api_to_server", "agent_to_server"])
+    rpc_server.run()
+    #t = T("top", ["bbb"])
