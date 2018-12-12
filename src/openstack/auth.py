c77c4fecee031ca7dfcaa29802e0857a28319dcf
jiangyt2112 <946692706@qq.com>
2018-12-12 17:26:12 +0800

    增加租户虚拟机信息查询

--- src/openstack/auth.py
+++ src/openstack/auth.py
@@ -4,19 +4,20 @@ from keystoneauth1 import session
 from keystoneclient.v3 import client
 from keystoneclient.v3 import tokens
 auth = v3.Password(auth_url='http://192.168.122.9:5000/v3',
-                   username='admin',
-                   password='e60ed34c828d44b9',
-                   project_name='admin',
+                   username='test',
+                   password='111111',
+                   project_name='test',
                    project_domain_name='Default',
                    user_domain_name='Default')
 sess = session.Session(auth=auth)
 auth_token = sess.get_token()
 print auth_token
 
-auth = v3.Token(auth_url='http://192.168.122.9:5000/v3', token = auth_token,project_domain_name='Default',project_name='admin')
+auth = v3.Token(auth_url='http://192.168.122.9:5000/v3', token = auth_token,project_domain_name='Default',project_name='test')
 sess = session.Session(auth=auth)
 
-keystone = client.Client(token = auth_token, session = sess, endpoint = 'http://192.168.122.9:35357/v3')
+keystone = client.Client(token = auth_token, auth_url = 'http://192.168.122.9:5000/v3', session = sess, endpoint = 'http://192.168.122.9:5000/v3')
 
 print keystone.authenticate()
+print keystone.users.list()
 print keystone.projects.list()
---
+++ src/openstack/nova.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python2
+from novaclient import client as nvclient
+from keystoneauth1.identity import v3
+from keystoneauth1 import session
+from keystoneclient.v3 import client
+from keystoneclient.v3 import tokens
+auth = v3.Password(auth_url='http://192.168.122.9:5000/v3',
+                   username='admin',
+                   password='e60ed34c828d44b9',
+                   project_name='admin',
+                   project_domain_name='Default',
+                   user_domain_name='Default')
+sess = session.Session(auth=auth)
+auth_token = sess.get_token()
+print auth_token
+kw = {}
+kw['project_domain_id'] = 'default'
+nova = nvclient.Client("2", auth_token=auth_token, auth_url='http://192.168.122.9:5000/v3',project_name='admin', **kw)
+print nova.servers.list()
