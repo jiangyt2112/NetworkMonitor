a7564cd6614d0e0216aedc0ca933265b35b51095
jiangyt2112 <946692706@qq.com>
2018-12-13 11:28:11 +0800

    修改虚拟机信息获取

--- src/openstack/auth.py
+++ src/openstack/auth.py
@@ -3,10 +3,7 @@ from keystoneauth1.identity import v3
 from keystoneauth1 import session
 from keystoneclient.v3 import client
 from keystoneclient.v3 import tokens
-auth_url = 'http://192.168.122.9:5000/v3'
-username = 'test'
-password = '111111'
-project_name = 'test'
+
 def get_token(username, password, auth_url, project_name):
 	auth = v3.Password(auth_url= auth_url,
 		               username=username,
@@ -15,7 +12,10 @@ def get_token(username, password, auth_url, project_name):
 		               project_domain_name='Default',
 		               user_domain_name='Default')
 	sess = session.Session(auth=auth)
-	auth_token = sess.get_token()
+	try:
+		auth_token = sess.get_token()
+	except Exception, e:
+		return None
 	return auth_token
 
 
@@ -33,5 +33,9 @@ def check_auth(auth_url, token, project_name):
 	return keystone.authenticate()
 
 if __name__ == "__main__":
+	auth_url = 'http://192.168.122.9:5000/v3'
+	username = 'test'
+	password = '111111'
+	project_name = 'test'
 	auth_token = get_token(username, password, auth_url, project_name)
 	print check_auth(auth_url, auth_token, project_name)
--- src/openstack/nova.py
+++ src/openstack/nova.py
@@ -4,20 +4,27 @@ from keystoneauth1.identity import v3
 from keystoneauth1 import session
 from keystoneclient.v3 import client
 from keystoneclient.v3 import tokens
-auth = v3.Password(auth_url='http://192.168.122.9:5000/v3',
-                   username='admin',
-                   password='e60ed34c828d44b9',
-                   project_name='admin',
-                   project_domain_name='Default',
-                   user_domain_name='Default')
-sess = session.Session(auth=auth)
-auth_token = sess.get_token()
-print auth_token
-kw = {}
-kw['project_domain_id'] = 'default'
-nova = nvclient.Client("2", auth_token=auth_token, auth_url='http://192.168.122.9:5000/v3',project_name='admin', **kw)
-print nova.servers.list()
-s = nova.servers.list()
-print s[0].__dict__
-print s[0]._info
-print s[0].to_dict()
+from auth import get_token
+
+
+def get_project_server_info(auth_token, auth_url, project_name):
+	kw = {}
+	kw['project_domain_id'] = 'default'
+	nova = nvclient.Client("2", auth_token=auth_token, auth_url='http://192.168.122.9:5000/v3',project_name='admin', **kw)
+	print nova.servers.list()
+	s = nova.servers.list()
+	print s[0].__dict__
+	print s[0]._info
+	print s[0].to_dict()
+
+if __name__ == '__main__':
+	auth_url = 'http://192.168.122.9:5000/v3'
+	username = 'test'
+	password = '111111'
+	project_name = 'test'
+	auth_token = get_token(username, password, auth_url, project_name)
+	if auth_token == None:
+		print 'auth fail.'
+		return
+	get_project_server_info(auth_token, auth_url, project_name)
+
