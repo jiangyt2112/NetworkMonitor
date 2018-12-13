700fd68357c238fa9eec03b2997509cae52a9502
jiangyt2112 <946692706@qq.com>
2018-12-13 18:05:18 +0800

    获取租户网络信息

--- src/openstack/neutron.py
+++ src/openstack/neutron.py
@@ -3,8 +3,10 @@ from neutronclient.v2_0 import client
 from auth import get_token
 
 def get_project_network_info(auth_token, auth_url):
-	neutron = client.Client(auth_url = auth_url,
-    	                	token = auth_token)
+	neutron = client.Client(auth_url = auth_url, token = auth_token)
+    print neutron.list_networks()
+    print neutron.list_subnets()
+    print neutron.list_ports()
 
 if __name__ == '__main__':
 	auth_url = 'http://192.168.122.9:5000/v3'
@@ -15,4 +17,4 @@ if __name__ == '__main__':
 	if auth_token == None:
 		print 'auth fail.'
 		exit(0)
-	get_project_network_info(auth_token, auth_url, project_name)
+	get_project_network_info(auth_token, auth_url)
