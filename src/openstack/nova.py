2a6d54d69e273c847b8c8b2da99eabbb3697497d
jiangyt2112 <946692706@qq.com>
2018-12-13 10:47:37 +0800

    修改获取虚拟机信息

--- src/openstack/nova.py
+++ src/openstack/nova.py
@@ -17,3 +17,7 @@ kw = {}
 kw['project_domain_id'] = 'default'
 nova = nvclient.Client("2", auth_token=auth_token, auth_url='http://192.168.122.9:5000/v3',project_name='admin', **kw)
 print nova.servers.list()
+s = nova.servers.list()
+print s[0].__dict__
+print s[0]._info
+print s[0].to_dict()
