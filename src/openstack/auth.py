db90f4f0f2defca80f02519b008c413bb4508a51
jiangyt2112 <946692706@qq.com>
2018-12-13 10:59:47 +0800

    修改鉴权

--- src/openstack/auth.py
+++ src/openstack/auth.py
@@ -3,21 +3,34 @@ from keystoneauth1.identity import v3
 from keystoneauth1 import session
 from keystoneclient.v3 import client
 from keystoneclient.v3 import tokens
-auth = v3.Password(auth_url='http://192.168.122.9:5000/v3',
-                   username='test',
-                   password='111111',
-                   project_name='test',
+auth_url = 'http://192.168.122.9:5000/v3'
+username = 'test'
+password = '111111'
+project_name = 'test'
+auth = v3.Password(auth_url= auth_url,
+                   username=username,
+                   password=password,
+                   project_name=project_name,
                    project_domain_name='Default',
                    user_domain_name='Default')
 sess = session.Session(auth=auth)
 auth_token = sess.get_token()
 print auth_token
 
-auth = v3.Token(auth_url='http://192.168.122.9:5000/v3', token = auth_token,project_domain_name='Default',project_name='test')
-sess = session.Session(auth=auth)
+#auth = v3.Token(auth_url='http://192.168.122.9:5000/v3', token = auth_token,project_domain_name='Default',project_name='test')
+#sess = session.Session(auth=auth)
 
-keystone = client.Client(token = auth_token, auth_url = 'http://192.168.122.9:5000/v3', session = sess, endpoint = 'http://192.168.122.9:5000/v3')
+#keystone = client.Client(token = auth_token, auth_url = 'http://192.168.122.9:5000/v3', session = sess, endpoint = 'http://192.168.122.9:5000/v3')
 
-print keystone.authenticate()
-print keystone.users.list()
-print keystone.projects.list()
+#print keystone.authenticate()
+#print keystone.users.list()
+#print keystone.projects.list()
+
+def check_auth(auth_url, token, project_name):
+	auth = v3.Token(auth_url=auth_url, token = token, project_domain_name='Default', project_name= project_name)
+	sess = session.Session(auth = auth)
+	keystone = client.Client(session = sess, endpoint = auth_url)
+	return keystone.authenticate()
+
+if __name__ == "__main__":
+	print check_auth(auth_url, auth_token, project_name)
