bda77d9d0f7f24401e76b8f38e4c2c38a7f894a4
jiangyt2112 <946692706@qq.com>
2018-12-14 09:33:23 +0800

    修改租户网络信息获取

--- src/openstack/neutron.py
+++ src/openstack/neutron.py
@@ -2,19 +2,20 @@
 from neutronclient.v2_0 import client
 from auth import get_token
 
-def get_project_network_info(auth_token, auth_url):
-	neutron = client.Client(auth_url = auth_url, token = auth_token)
+def get_project_network_info(auth_token, auth_url, endpoint):
+	neutron = client.Client(auth_url = auth_url, token = auth_token, endpoint_url = endpoint)
 	print neutron.list_networks()
 	print neutron.list_subnets()
 	print neutron.list_ports()
 
 if __name__ == '__main__':
 	auth_url = 'http://192.168.122.9:5000/v3'
-	username = 'test'
-	password = '111111'
-	project_name = 'test'
+	endpoint_url = 'http://192.168.122.9:9696'
+	username = 'admin'
+	password = 'e60ed34c828d44b9'
+	project_name = 'admin'
 	auth_token = get_token(username, password, auth_url, project_name)
 	if auth_token == None:
 		print 'auth fail.'
 		exit(0)
-	get_project_network_info(auth_token, auth_url)
+	get_project_network_info(auth_token, auth_url, endpoint_url)
