170e1ef003203baffe8e49797ace0c92c3d57737
jiangyt2112 <946692706@qq.com>
2018-12-20 09:34:39 +0800

    实现api功能

--- src/api/func.py
+++ src/api/func.py
@@ -1,23 +1,40 @@
 #!/usr/bin/python2
 import __init__
 from comm.client import api_to_server_msg
+from utils.conf import CONF
+from openstack.auth import check_auth as os_check_auth
+from comm.client import api_to_server_msg
 
-def check_auth(project_name, token):
-	pass
-
+def check_auth(msg):
+	auth_url = CONF.openstack_conf['auth_url']
+	return os_check_auth(auth_url, msg['token'], msg['project_name'])
 
 def check_msg(msg):
-	# 授权
-	# 数据库
-	# 通信
-    # response = {
-    #         'task_type': 'start_re',
-    #         'exe_result': False,
-    #         'req_id': req_id,
-    #         'vm_id': vm_id,
-    #         'error_msg': msg 
-    #         }
-	pass
+	response = {
+            'task_type': 'check',
+            'exe_result': False,
+            'req_id': msg['req_id'],
+            'project': msg['project_name'],
+            'result': None,
+            'error_msg': None 
+            }
+	# check auth
+	if check_auth(msg) == False:
+		response['error_msg'] = 'no authorization'
+		return response
+
+	# database
+	# 数据库查和创建任务
+
+	# dispatch task to back end
+	task = {'type': 'check', 'req_id': msg['req_id'], 'project': msg['project_name'], 'token': msg['token']}
+	ret = api_to_server_msg(task)
+	if ret[0] == False:
+		response['error_msg'] = ret[1]
+	else:
+		response['exe_result'] = True
+		response['result'] = 'run check task'
+	return response
 
 def get_status(msg):
 	# 授权
