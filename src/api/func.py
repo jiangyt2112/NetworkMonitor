bf33572836ad79909ef8dbd84a9c90e3e817edfd
jiangyt2112 <946692706@qq.com>
2019-02-21 10:25:37 +0800

    debug

--- src/api/func.py
+++ src/api/func.py
@@ -2,7 +2,7 @@
 import __init__
 from comm.client import api_to_server_msg
 from utils.conf import CONF
-from openstack.auth import check_auth as os_check_auth
+from openstackapi.auth import check_auth as os_check_auth
 from comm.client import api_to_server_msg
 
 def check_auth(msg):
