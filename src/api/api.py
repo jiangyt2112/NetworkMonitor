571131ede7d07b5726e5c01293da557094e18d1d
jiangyt2112 <946692706@qq.com>
2019-02-28 22:02:25 +0800

    debug

--- src/api/api.py
+++ src/api/api.py
@@ -169,7 +169,7 @@ class NetworkMonitorHistory(Resource):
 
 
 # api.add_resource(HelloWorld, '/')
-api.add_resource(NetworkMonitorCheck, '/network_monitor/ckeck/<string:project_name>')
+api.add_resource(NetworkMonitorCheck, '/network_monitor/check/<string:project_name>')
 api.add_resource(NetworkMonitorStatus, '/network_monitor/status/<string:project_name>')
 api.add_resource(NetworkMonitorResult, '/network_monitor/result/<string:project_name>')
 api.add_resource(NetworkMonitorHistory, '/network_monitor/history/<string:project_name>')
