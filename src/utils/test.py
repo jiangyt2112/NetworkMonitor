ce4c09741f13616ab1d4ee56523b1698c2831f99
jiangyt2112 <946692706@qq.com>
2019-03-02 11:51:59 +0800

    ovs数据获取

--- src/utils/test.py
+++ src/utils/test.py
@@ -1,39 +1,6 @@
-
-from itertools import chain
-from ovs import ovsdb
-from ovs.utils import execute
-from ovs.utils import decorator
-
-def show_br(self):
-    brs, br = {}, ''
-    cmd = 'ovs-vsctl show'
-    result, error = execute.exec_cmd(cmd)
-    if error:
-        return {}
-    for l in result.split('\n'):
-        l = l.strip().replace('"', '')
-        if l.startswith('Bridge '):
-            br = l.lstrip('Bridge ')
-            brs[br] = {}
-            brs[br]['Controller'] = []
-            brs[br]['Port'] = {}
-            brs[br]['fail_mode'] = ''
-        else:
-            if l.startswith('Controller '):
-                brs[br]['Controller'].append(l.replace('Controller ', ''))
-            elif l.startswith('fail_mode: '):
-                brs[br]['fail_mode'] = l.replace('fail_mode: ', '')
-            elif l.startswith('Port '):
-                phy_port = l.replace('Port ', '')  # e.g., br-eth0
-                brs[br]['Port'][phy_port] = {'vlan': '', 'type': ''}
-            elif l.startswith('tag: '):
-                brs[br]['Port'][phy_port]['vlan'] = l.replace('tag: ', '')
-            elif l.startswith('Interface '):
-                brs[br]['Port'][phy_port]['intf'] = \
-                    l.replace('Interface ', '')
-            elif l.startswith('type: '):
-                brs[br]['Port'][phy_port]['type'] = l.replace('type: ', '')
-    return brs
+#!/usr/bin/python2
+from ovs import bridge
+br = Bridge()
 
 if __name__ == '__main__':
-    print show_br()
+    print br.show_br()
