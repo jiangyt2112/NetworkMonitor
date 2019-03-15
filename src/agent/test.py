5e175252b954dfad80085d461163a9c22592b154
jiangyt2112 <946692706@qq.com>
2019-03-15 09:33:41 +0800

    debug

--- src/agent/test.py
+++ src/agent/test.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python2
 import __init__
 from func import exe
-from func import process_tap_info
-info = exe("ip netns exec qrouter-d4edac45-231a-4b5e-9e95-c629d5c7fc62 ip addr show qg-b8cfeaad-ef")
-info = info[1]
-print process_tap_info(info)
+from func import get_bridge_info
+# info = exe("ip netns exec qrouter-d4edac45-231a-4b5e-9e95-c629d5c7fc62 ip addr show qg-b8cfeaad-ef")
+# info = info[1]
+# print process_tap_info(info)
+
+print get_bridge_info("qbr3ef787ad-67")
+
+print get_bridge_info("qbr3ef787ad-68")
