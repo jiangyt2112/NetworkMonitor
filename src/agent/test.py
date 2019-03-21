ab08c15fa9e726762097e2d6b0ec150f4bca9e47
jiangyt2112 <946692706@qq.com>
2019-03-21 10:30:27 +0800

    debug

--- src/agent/test.py
+++ src/agent/test.py
@@ -25,14 +25,14 @@ except ImportError:
 
 def get_key():
  
-    key_info = psutil.net_io_counters(pernic=True).keys()  # 获取网卡名称
+    key_info = psutil.net_io_counters(pernic=True).keys()
  
     recv = {}
     sent = {}
  
     for key in key_info:
-        recv.setdefault(key, psutil.net_io_counters(pernic=True).get(key).bytes_recv)  # 各网卡接收的字节数
-        sent.setdefault(key, psutil.net_io_counters(pernic=True).get(key).bytes_sent)  # 各网卡发送的字节数
+        recv.setdefault(key, psutil.net_io_counters(pernic=True).get(key).bytes_recv)
+        sent.setdefault(key, psutil.net_io_counters(pernic=True).get(key).bytes_sent)
  
     return key_info, recv, sent
 
@@ -41,18 +41,17 @@ def get_rate(func):
  
     import time
  
-    key_info, old_recv, old_sent = func()  # 上一秒收集的数据
+    key_info, old_recv, old_sent = func()
  
     time.sleep(1)
  
-    key_info, now_recv, now_sent = func()  # 当前所收集的数据
- 
+    key_info, now_recv, now_sent = func()
     net_in = {}
     net_out = {}
  
     for key in key_info:
-        net_in.setdefault(key, (now_recv.get(key) - old_recv.get(key)) / 1024)  # 每秒接收速率
-        net_out.setdefault(key, (now_sent.get(key) - old_sent.get(key)) / 1024) # 每秒发送速率
+        net_in.setdefault(key, (now_recv.get(key) - old_recv.get(key)) / 1024)
+        net_out.setdefault(key, (now_sent.get(key) - old_sent.get(key)) / 1024)
  
     return key_info, net_in, net_out
  
