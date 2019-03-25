ae54f31d9d67e3d9cc3f80cd8a7a57e490934e8a
jiangyt2112 <946692706@qq.com>
2019-03-25 19:34:46 +0800

    debug

--- src/agent/test.py
+++ src/agent/test.py
@@ -31,46 +31,46 @@ from openstackapi.neutron import get_project_network_info
 # print get_host_ip()
 # print is_network_node()
 
-# from func import get_port_network_info
-# auth_url = 'http://192.168.122.9:5000/v3'
-# endpoint_url = 'http://192.168.122.9:9696'
-# username = 'admin'
-# password = 'e60ed34c828d44b9'
-# project_name = 'admin'
-# auth_token = get_token(username, password, auth_url, project_name)
-# if auth_token == None:
-# 	print 'auth fail.'
-# 	exit(0)
-# network_info = get_project_network_info(auth_token, auth_url, endpoint_url)
+from func import get_port_network_info
+auth_url = 'http://192.168.122.9:5000/v3'
+endpoint_url = 'http://192.168.122.9:9696'
+username = 'admin'
+password = 'e60ed34c828d44b9'
+project_name = 'admin'
+auth_token = get_token(username, password, auth_url, project_name)
+if auth_token == None:
+	print 'auth fail.'
+	exit(0)
+network_info = get_project_network_info(auth_token, auth_url, endpoint_url)
 # # print get_port_network_info(network_info['ports'][0], network_info)
 # from libvirt_func import get_vm_port_netinfo, get_vm_port_netstats, get_nic_netstats
 # # print get_vm_port_netinfo()
 # vm_port_netstats = get_vm_port_netstats()
 # get_nic_netstats()
 
-# from novaclient import client as nvclient
-# from openstackapi.auth import get_token
-# auth_url = 'http://192.168.122.9:5000/v3'
-# username = 'admin'
-# password = 'e60ed34c828d44b9'
-# project_name = 'admin'
-# auth_token = get_token(username, password, auth_url, project_name)
-# from openstackapi.nova import get_project_server_info
-# vm_info = get_project_server_info(auth_token, auth_url, project_name)
+from novaclient import client as nvclient
+from openstackapi.auth import get_token
+auth_url = 'http://192.168.122.9:5000/v3'
+username = 'admin'
+password = 'e60ed34c828d44b9'
+project_name = 'admin'
+auth_token = get_token(username, password, auth_url, project_name)
+from openstackapi.nova import get_project_server_info
+vm_info = get_project_server_info(auth_token, auth_url, project_name)
 
-topo = {
-		"device": [],
-		"tap":[],
-		"qbr":[],
-		"qvb":[],
-		"qvo":[],
-		"br-int-port":[],
-		"br-int":[],
-		"ovs-provider":[],
-		"nic":[],
-		"physical-switch":[]
-		}
-touch_ips = set()
+# topo = {
+# 		"device": [],
+# 		"tap":[],
+# 		"qbr":[],
+# 		"qvb":[],
+# 		"qvo":[],
+# 		"br-int-port":[],
+# 		"br-int":[],
+# 		"ovs-provider":[],
+# 		"nic":[],
+# 		"physical-switch":[]
+# 		}
+# touch_ips = set()
 # from func import get_vm_topo
 # for vm in vm_info:
 # 	get_vm_topo(vm, network_info, topo, touch_ips, vm_port_netstats)
@@ -89,6 +89,8 @@ from func import get_nic_ex_info, get_nic_tun_ip, get_nic_tun_info, get_tunnel_r
 # ip = get_nic_tun_ip()
 # info = {'ip_address': ip[1]}
 # print get_nic_tun_info(info)
-from ovs.bridge import get_ovs_info
-ret, br_info = get_ovs_info()
-print get_tunnel_remote(br_info['br-tun'])
+# from ovs.bridge import get_ovs_info
+# ret, br_info = get_ovs_info()
+# print get_tunnel_remote(br_info['br-tun'])
+from func import get_topo
+get_topo(vms_info, networks_info)
