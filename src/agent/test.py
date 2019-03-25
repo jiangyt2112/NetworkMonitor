8039d34f606c5e9e81c8a2851368695bd038fcbc
jiangyt2112 <946692706@qq.com>
2019-03-25 15:09:46 +0800

    debug

--- src/agent/test.py
+++ src/agent/test.py
@@ -6,6 +6,9 @@ from func import ping_test
 from func import get_nic_info
 from func import get_vm_uuids, get_hostname, check_service, get_host_ip, is_network_node
 from libvirt_func import get_vm_port_netinfo, get_nic_netstats
+
+from openstackapi import get_project_network_info
+
 # info = exe("ip netns exec qrouter-d4edac45-231a-4b5e-9e95-c629d5c7fc62 ip addr show qg-b8cfeaad-ef")
 # info = info[1]
 # print process_tap_info(info)
@@ -20,8 +23,20 @@ from libvirt_func import get_vm_port_netinfo, get_nic_netstats
 # print get_vm_port_netinfo()
 # print get_nic_netstats()
 # print get_vm_uuids()
-print get_hostname()
-print check_service("openvswitch")
-print check_service("opnevswitch")
-print get_host_ip()
-print is_network_node()
+# print get_hostname()
+# print check_service("openvswitch")
+# print check_service("opnevswitch")
+# print get_host_ip()
+# print is_network_node()
+
+
+auth_url = 'http://192.168.122.9:5000/v3'
+endpoint_url = 'http://192.168.122.9:9696'
+username = 'admin'
+password = 'e60ed34c828d44b9'
+project_name = 'admin'
+auth_token = get_token(username, password, auth_url, project_name)
+if auth_token == None:
+	print 'auth fail.'
+	exit(0)
+get_project_network_info(auth_token, auth_url, endpoint_url)
