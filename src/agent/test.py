03a0d48a7fc22e0a96814076b2a4dfb8aea7b361
jiangyt2112 <946692706@qq.com>
2019-03-25 11:17:39 +0800

    debug

--- src/agent/test.py
+++ src/agent/test.py
@@ -4,6 +4,7 @@ from func import exe
 from func import get_bridge_info
 from func import ping_test
 from func import get_nic_info
+from func import get_vm_uuids, get_hostname, check_service
 from libvirt_func import get_vm_port_netinfo, get_nic_netstats
 # info = exe("ip netns exec qrouter-d4edac45-231a-4b5e-9e95-c629d5c7fc62 ip addr show qg-b8cfeaad-ef")
 # info = info[1]
@@ -17,4 +18,8 @@ from libvirt_func import get_vm_port_netinfo, get_nic_netstats
 # print get_nic_info("br-ex")
 # print get_nic_info("ens5")
 # print get_vm_port_netinfo()
-print get_nic_netstats()
+# print get_nic_netstats()
+print get_vm_uuids()
+print get_hostname()
+print check_service("openvswitch")
+print check_service("opnevswitch")
