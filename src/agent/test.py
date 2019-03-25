c0d01e4c58a1b77014e9db08190c69876451d5f6
jiangyt2112 <946692706@qq.com>
2019-03-25 15:31:07 +0800

    debug

--- src/agent/test.py
+++ src/agent/test.py
@@ -31,19 +31,46 @@ from openstackapi.neutron import get_project_network_info
 # print get_host_ip()
 # print is_network_node()
 
-# from func import get_port_network_info
-# auth_url = 'http://192.168.122.9:5000/v3'
-# endpoint_url = 'http://192.168.122.9:9696'
-# username = 'admin'
-# password = 'e60ed34c828d44b9'
-# project_name = 'admin'
-# auth_token = get_token(username, password, auth_url, project_name)
-# if auth_token == None:
-# 	print 'auth fail.'
-# 	exit(0)
-# network_info = get_project_network_info(auth_token, auth_url, endpoint_url)
+from func import get_port_network_info
+auth_url = 'http://192.168.122.9:5000/v3'
+endpoint_url = 'http://192.168.122.9:9696'
+username = 'admin'
+password = 'e60ed34c828d44b9'
+project_name = 'admin'
+auth_token = get_token(username, password, auth_url, project_name)
+if auth_token == None:
+	print 'auth fail.'
+	exit(0)
+network_info = get_project_network_info(auth_token, auth_url, endpoint_url)
 # print get_port_network_info(network_info['ports'][0], network_info)
 from libvirt_func import get_vm_port_netinfo, get_vm_port_netstats, get_nic_netstats
 # print get_vm_port_netinfo()
-# print get_vm_port_netstats()
-print get_nic_netstats()
+vm_port_netstats = get_vm_port_netstats()
+# get_nic_netstats()
+
+from novaclient import client as nvclient
+from auth import get_token
+auth_url = 'http://192.168.122.9:5000/v3'
+username = 'admin'
+password = 'e60ed34c828d44b9'
+project_name = 'admin'
+auth_token = get_token(username, password, auth_url, project_name)
+if auth_token == None:
+print 'auth fail.'
+
+vm_info = get_project_server_info(auth_token, auth_url, project_name)
+
+topo = {
+		"device": [],
+		"tap":[],
+		"qbr":[],
+		"qvb":[],
+		"qvo":[],
+		"br-int-port":[],
+		"br-int":[],
+		"ovs-provider":[],
+		"nic":[],
+		"physical-switch":[]
+		}
+touch_ips = set()
+get_vm_topo(vm_info, network_info, topo, touch_ips, vm_port_netstats)
