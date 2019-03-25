3b061eaa08d2a3fcfe805c6e4b60a9faa2e00d29
jiangyt2112 <946692706@qq.com>
2019-03-25 20:31:28 +0800

    debug

--- src/agent/test.py
+++ src/agent/test.py
@@ -101,5 +101,5 @@ topo = get_topo(vms_info, networks_info)
 # print process_tap_info(info)
 # ret, info = exe("ip addr show %s" %("br-ex"))
 # print process_tap_info(info)
-
+from func import check_br_int_port
 check_br_int_port(topo['br-int-port'][0], topo)
