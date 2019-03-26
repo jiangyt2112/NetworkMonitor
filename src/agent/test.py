8405272c488bead67831b602802af73c4b1461d7
jiangyt2112 <946692706@qq.com>
2019-03-26 17:02:23 +0800

    debug

--- src/agent/test.py
+++ src/agent/test.py
@@ -135,5 +135,6 @@ from func import bond_tap_addr, ping_test
 
 from func import is_connect_internal, check_device_connection, check_nic_connection
 # print is_connect_internal('1234567890123','192.168.1.8', '24', '1')
-check_device_connection(topo['device'][0], topo)
-check_nic_connection(topo['nic'][0], topo)
+# check_device_connection(topo['device'][0], topo)
+# check_nic_connection(topo['nic'][0], topo)
+check_network_connection(topo)
