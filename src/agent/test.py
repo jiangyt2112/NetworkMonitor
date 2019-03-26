8cc041aa9d39bc3034f9f989015f5260a142371c
jiangyt2112 <946692706@qq.com>
2019-03-26 14:57:18 +0800

    debug

--- src/agent/test.py
+++ src/agent/test.py
@@ -117,7 +117,7 @@ print len(topo['device'])
 for i in topo['device']:
 	print i['name']
 from func import check_network_ovs
-from func import check_network_nic
+from func import check_network_nic, check_network_config
 # check_network_device(topo)
 # check_network_ovs(topo)
 # check_network_nic(topo)
