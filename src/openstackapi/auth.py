019dfb8cde650ef23c21c5e66e133a5f2d5d0171
jiangyt2112 <946692706@qq.com>
2019-04-08 09:34:16 +0800

    debug

--- src/openstackapi/auth.py
+++ src/openstackapi/auth.py
@@ -41,9 +41,9 @@ def check_auth(auth_url, auth_token, project_name):
 	return ret
 
 if __name__ == "__main__":
-	auth_url = 'http://192.168.122.9:5000/v3'
+	auth_url = 'http://192.168.122.2:5000/v3'
 	username = 'admin'
-	password = 'e60ed34c828d44b9'
+	password = '1787f5020b4d451e'
 	project_name = 'admin'
 	auth_token = get_token(username, password, auth_url, project_name)
 	print auth_token
